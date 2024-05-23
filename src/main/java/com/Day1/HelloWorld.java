d9184a9a76284735e443c708dfc1d94784d9a804
Manasa <manasa.kiran201407@gmail.com>
2024-05-23 16:55:22 +0530

    added day1 folder

---
+++ src/main/java/com/Day1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.Day1;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
