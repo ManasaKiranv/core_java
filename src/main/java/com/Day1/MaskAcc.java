c9badc271359db407604675af3027427f6e75033
KIRAN KUMAR VOODARI <kirankumarvoodari@KIRANs-MacBook-Pro.local>
2024-05-24 12:04:46 +0530

    added code for checking null, empty filters

--- src/main/java/com/Day1/MaskAcc.java
+++ src/main/java/com/Day1/MaskAcc.java
@@ -1,4 +1,7 @@
 package com.Day1;
+
+import org.apache.commons.lang3.StringUtils;
+
 // hide all digits of account number except last 4 digits
 public final class MaskAcc {
     
@@ -6,12 +9,14 @@ public final class MaskAcc {
         System.out.println("constructor excuted");
     }
     public String maskAvv(String Accno){
-        return "********" + Accno.substring(Accno.length()-4);
+        if(StringUtils.isNotEmpty(Accno) && (Accno.length()>4))
+            return "********" + Accno.substring(Accno.length()-4);
+        return "not valid Accunt number";
     }
     public static void main(String[] args) {
         
     MaskAcc ma = new MaskAcc();
-    String maskedAccno = ma.maskAvv("123456789987");
+    String maskedAccno = ma.maskAvv("12353437272783278");
     System.out.println(maskedAccno);
     System.out.println("successfully masked your account number");
     }
